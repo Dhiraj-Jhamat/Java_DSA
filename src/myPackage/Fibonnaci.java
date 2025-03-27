591f1b127690cc0ed113e2a4f4c37af1c9cd1340
Dhiraj-Jhamat <dhirajjhamat@gmail.com>
2025-03-27 09:56:18 +0530

    modifying Fibonnaci file

--- src/myPackage/Fibonnaci.java
+++ src/myPackage/Fibonnaci.java
@@ -1,4 +1,21 @@
 package myPackage;
 
+import java.util.Scanner;
+
 public class Fibonnaci {
+    public static void main(String[] args) {
+        System.out.print(" Enter the nth number: ");
+        Scanner sc = new Scanner( System.in );
+        int n = sc.nextInt();
+        int a=0, b=1;
+        int temp;
+        int index=2;
+        while(index<=n){
+            temp=b;
+            b+=a;
+            a=temp;
+            index++;
+        }
+        System.out.println(b);
+    }
 }
